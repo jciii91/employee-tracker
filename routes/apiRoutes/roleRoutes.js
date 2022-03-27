f7ff52d50a4e9b8f87913c2dc3d9e4bdfdf18909
JCIII <jcostello711@gmail.com>
2022-03-27 18:32:58 -0400

    fixed roles table formatting. department_id number replaced with department_name

--- routes/apiRoutes/roleRoutes.js
+++ routes/apiRoutes/roleRoutes.js
@@ -4,7 +4,11 @@ const db = require('../../db/connection');
 
 // Get all roles
 router.get('/roles', (req, res) => {
-    const sql = 'SELECT * FROM roles';
+    const sql = `SELECT roles.id, roles.title, roles.salary, departments.name
+                    AS department_name
+                    FROM roles
+                    LEFT JOIN departments
+                    ON roles.department_id = departments.id`;
 
     db.query(sql, (err, rows) => {
     if (err) {
