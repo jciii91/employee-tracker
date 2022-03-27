6267dee7c9df8bd6d3b9bae9178c7f4247869d56
JCIII <jcostello711@gmail.com>
2022-03-27 12:42:21 -0400

    main menu added. inquirer list question used to get initial action selection from user

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
+const { mainMenu } = require('./lib/mainMenu');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -23,5 +24,6 @@ db.connect(err => {
     console.log('Database connected.');
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
+        mainMenu();
     });
 });
