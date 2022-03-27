d36861f102a640a17ecdfe36813c75853ba162eb
JCIII <jcostello711@gmail.com>
2022-03-27 15:41:57 -0400

    all view options return data from the correct tables

--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -2,4 +2,20 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
 
+// Get all employees
+router.get('/employees', (req, res) => {
+    const sql = 'SELECT * FROM employees';
+
+    db.query(sql, (err, rows) => {
+    if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+    }
+    res.json({
+        message: 'success',
+        data: rows
+        });
+    });
+});
+
 module.exports = router;
